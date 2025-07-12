a9e2d3576faa8b7a493dc01ff749add1a59ca7ea
Vasyl Linkin <linkinmvv@gmail.com>
2025-07-12 21:04:30 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const formEl = document.querySelector("form.login-form");
+formEl.addEventListener("submit", handleFormSubmit);
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  if (formEl.email.value === "" || formEl.password.value === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = new FormData(formEl);
+    const userInfo = Object.fromEntries(formData.entries());
+    console.table(userInfo);
+  }
+
+  formEl.reset();
+}
